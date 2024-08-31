60575a6647315a5739ba737e5b034f31cb96519b
SpikeSpieg-el <112814519+SpikeSpieg-el@users.noreply.github.com>
2024-09-01 01:38:01 +0500

    проба

--- index.js
+++ index.js
@@ -1,47 +1,20 @@
 vkBridge.send("VKWebAppInit", {})
-    .then(() => {
-        console.log('VK Bridge инициализирован');
+        .then(() => {
+            console.log('VK Bridge инициализирован');
     })
     .catch((error) => {
         console.error('Ошибка инициализации VK Bridge:', error);
-    });
+});
 
-vkBridge.send('VKWebAppShowNativeAds', {
-        ad_format: 'interstitial' // Тип рекламы
-    })
-    .then((data) => { 
-        if (data.result) {
-            // Реклама была показана
-        } else {
-            // Ошибка
-        }
-    })
-    .catch((error) => { 
-        console.log(error); 
-    });
-
-vkBridge.send('VKWebAppShowBannerAd', {
-        banner_location: 'bottom'
-    })
-    .then((data) => { 
-        if (data.result) {
-            // Баннерная реклама отобразилась
-        }
-    })
-    .catch((error) => {
-        // Ошибка
-        console.log(error);
-    });
-
-vkBridge.send('VKWebAppShowBannerAd', {
+    vkBridge.send('VKWebAppShowBannerAd', {
         banner_location: 'top'
-    })
-    .then((data) => { 
-        if (data.result) {
+        })
+       .then((data) => { 
+          if (data.result) {
             // Баннерная реклама отобразилась
-        }
-    })
-    .catch((error) => {
-        // Ошибка
-        console.log(error);
-    });
+          }
+        })
+        .catch((error) => {
+          // Ошибка
+          console.log(error);
+        });
