b912e160661d23d8c1a74231bf0344e56359bdc8
SpikeSpieg-el <112814519+SpikeSpieg-el@users.noreply.github.com>
2024-09-01 00:04:38 +0500

    банеры

--- index.js
+++ index.js
@@ -2,6 +2,58 @@ vkBridge.send("VKWebAppInit", {})
     .then(() => {
         console.log('VK Bridge инициализирован');
 
+        // Отображение баннера сверху
+        vkBridge.send('VKWebAppShowBannerAd', {
+            banner_location: 'top'
+        })
+        .then((data) => { 
+            if (data.result) {
+                console.log('Баннерная реклама отобразилась сверху');
+            }
+        })
+        .catch((error) => {
+            console.log('Ошибка отображения баннера сверху:', error);
+        });
+
+        // Отображение баннера снизу
+        vkBridge.send('VKWebAppShowBannerAd', {
+            banner_location: 'bottom'
+        })
+        .then((data) => { 
+            if (data.result) {
+                console.log('Баннерная реклама отобразилась снизу');
+            }
+        })
+        .catch((error) => {
+            console.log('Ошибка отображения баннера снизу:', error);
+        });
+
+        // Отображение баннера слева
+        vkBridge.send('VKWebAppShowBannerAd', {
+            banner_location: 'left'
+        })
+        .then((data) => { 
+            if (data.result) {
+                console.log('Баннерная реклама отобразилась слева');
+            }
+        })
+        .catch((error) => {
+            console.log('Ошибка отображения баннера слева:', error);
+        });
+
+        // Отображение баннера справа
+        vkBridge.send('VKWebAppShowBannerAd', {
+            banner_location: 'right'
+        })
+        .then((data) => { 
+            if (data.result) {
+                console.log('Баннерная реклама отобразилась справа');
+            }
+        })
+        .catch((error) => {
+            console.log('Ошибка отображения баннера справа:', error);
+        });
+
         // Устанавливаем таймер на 15 секунд
         setTimeout(() => {
             // Проверка готовности рекламы
@@ -29,7 +81,7 @@ vkBridge.send("VKWebAppInit", {})
                 .catch((error) => {
                     console.log('Ошибка при проверке готовности рекламы:', error);
                 });
-        }, 1500); // 15000 мс = 15 секунд
+        }, 15000); // 15000 мс = 15 секунд
 
     })
     .catch((error) => {
