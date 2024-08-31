ff691d332bef70418d5287794b7e120a26cf4851
SpikeSpieg-el <112814519+SpikeSpieg-el@users.noreply.github.com>
2024-09-01 01:20:17 +0500

    адд проверка

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 vkBridge.send("VKWebAppInit", {})
     .then(() => {
         console.log('VK Bridge инициализирован');
-        // Запуск показа рекламы через 15 секунд
-        setTimeout(() => {
-            vkBridge.send('VKWebAppShowNativeAds', { ad_format: 'reward' })
-                .then((data) => { 
-                    if (data.result) {
-                        console.log('Реклама показана');
-                        // Обновление страницы после показа рекламы
-                        location.reload();
-                    } else {
-                        console.log('Ошибка при показе рекламы');
-                    }
-                })
-                .catch((error) => {
-                    console.log('Ошибка:', error);
-                });
-        }, 15000); // 15000 миллисекунд = 15 секунд
     })
     .catch((error) => {
         console.error('Ошибка инициализации VK Bridge:', error);
     });
+    bridge.send('VKWebAppShowNativeAds', {
+        ad_format: 'interstitial' /* Тип рекламы */
+        })
+        .then( (data) => { 
+          if (data.result) {
+            // Реклама была показана
+          } else {
+            // Ошибка
+          }
+        })
+        .catch((error) => { console.log(error); });
+
+        bridge.send('VKWebAppShowBannerAd', {
+            banner_location: 'bottom'
+            })
+           .then((data) => { 
+              if (data.result) {
+                // Баннерная реклама отобразилась
+              }
+            })
+            .catch((error) => {
+              // Ошибка
+              console.log(error);
+            });
+            bridge.send('VKWebAppShowBannerAd', {
+                banner_location: 'top'
+                })
+               .then((data) => { 
+                  if (data.result) {
+                    // Баннерная реклама отобразилась
+                  }
+                })
+                .catch((error) => {
+                  // Ошибка
+                  console.log(error);
+                });
