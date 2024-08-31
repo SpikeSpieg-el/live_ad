2ed438d435bc1397f0836864cc15e1695d65455e
SpikeSpieg-el <112814519+SpikeSpieg-el@users.noreply.github.com>
2024-09-01 01:30:28 +0500

    пробуем

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 vkBridge.send("VKWebAppInit", {})
     .then(() => {
         console.log('VK Bridge инициализирован');
+        
+        return vkBridge.send('VKWebAppShowNativeAds', {
+            ad_format: 'interstitial' // Тип рекламы
+        });
+    })
+    .then((data) => { 
+        if (data.result) {
+            // Реклама была показана
+        } else {
+            // Ошибка
+        }
     })
     .catch((error) => {
-        console.error('Ошибка инициализации VK Bridge:', error);
+        console.error('Ошибка:', error);
     });
-    vkBridge.send('VKWebAppShowNativeAds', {
-        ad_format: 'interstitial' /* Тип рекламы */
-        })
-        .then( (data) => { 
-          if (data.result) {
-            // Реклама была показана
-          } else {
-            // Ошибка
-          }
-        })
-        .catch((error) => { console.log(error); });
 
-        vkBridge.send('VKWebAppShowBannerAd', {
-            banner_location: 'bottom'
-            })
-           .then((data) => { 
-              if (data.result) {
-                // Баннерная реклама отобразилась
-              }
-            })
-            .catch((error) => {
-              // Ошибка
-              console.log(error);
-            });
-            vkBridge.send('VKWebAppShowBannerAd', {
-                banner_location: 'top'
-                })
-               .then((data) => { 
-                  if (data.result) {
-                    // Баннерная реклама отобразилась
-                  }
-                })
-                .catch((error) => {
-                  // Ошибка
-                  console.log(error);
-                });
+// Показ баннерной рекламы
+vkBridge.send('VKWebAppShowBannerAd', {
+        banner_location: 'bottom'
+    })
+    .then((data) => { 
+        if (data.result) {
+            // Баннерная реклама отобразилась (внизу)
+        }
+    })
+    .catch((error) => {
+        console.error('Ошибка показа нижнего баннера:', error);
+    });
+
+vkBridge.send('VKWebAppShowBannerAd', {
+        banner_location: 'top'
+    })
+    .then((data) => { 
+        if (data.result) {
+            // Баннерная реклама отобразилась (вверху)
+        }
+    })
+    .catch((error) => {
+        console.error('Ошибка показа верхнего баннера:', error);
+    });
