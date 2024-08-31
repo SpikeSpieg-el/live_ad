30454f72e7f86c2e7ca0b026322d141350199047
SpikeSpieg-el <112814519+SpikeSpieg-el@users.noreply.github.com>
2024-09-01 00:19:30 +0500

    пробуем

--- index.js
+++ index.js
@@ -1,88 +1,22 @@
 vkBridge.send("VKWebAppInit", {})
     .then(() => {
         console.log('VK Bridge инициализирован');
-
-        // Отображение баннера сверху
-        vkBridge.send('VKWebAppShowBannerAd', {
-            banner_location: 'top'
-        })
-        .then((data) => { 
-            if (data.result) {
-                console.log('Баннерная реклама отобразилась сверху');
-            }
-        })
-        .catch((error) => {
-            console.log('Ошибка отображения баннера сверху:', error);
-        });
-
-        // Отображение баннера снизу
-        vkBridge.send('VKWebAppShowBannerAd', {
-            banner_location: 'bottom'
-        })
-        .then((data) => { 
-            if (data.result) {
-                console.log('Баннерная реклама отобразилась снизу');
-            }
-        })
-        .catch((error) => {
-            console.log('Ошибка отображения баннера снизу:', error);
-        });
-
-        // Отображение баннера слева
-        vkBridge.send('VKWebAppShowBannerAd', {
-            banner_location: 'left'
-        })
-        .then((data) => { 
-            if (data.result) {
-                console.log('Баннерная реклама отобразилась слева');
-            }
-        })
-        .catch((error) => {
-            console.log('Ошибка отображения баннера слева:', error);
-        });
-
-        // Отображение баннера справа
-        vkBridge.send('VKWebAppShowBannerAd', {
-            banner_location: 'right'
-        })
-        .then((data) => { 
-            if (data.result) {
-                console.log('Баннерная реклама отобразилась справа');
-            }
-        })
-        .catch((error) => {
-            console.log('Ошибка отображения баннера справа:', error);
-        });
-
-        // Устанавливаем таймер на 15 секунд
+        // Запуск показа рекламы через 15 секунд
         setTimeout(() => {
-            // Проверка готовности рекламы
-            vkBridge.send('VKWebAppCheckNativeAds', { ad_format: 'reward' })
-                .then((data) => {
+            vkBridge.send('VKWebAppShowNativeAds', { ad_format: 'reward' })
+                .then((data) => { 
                     if (data.result) {
-                        // Реклама готова, показываем ее
-                        vkBridge.send('VKWebAppShowNativeAds', { ad_format: 'reward' })
-                            .then((data) => {
-                                if (data.result) {
-                                    console.log('Реклама показана');
-                                    // Обновляем страницу после показа рекламы
-                                    location.reload();
-                                } else {
-                                    console.log('Ошибка при показе рекламы');
-                                }
-                            })
-                            .catch((error) => {
-                                console.log('Ошибка при показе рекламы:', error);
-                            });
+                        console.log('Реклама показана');
+                        // Обновление страницы после показа рекламы
+                        location.reload();
                     } else {
-                        console.log('Рекламные материалы не найдены.');
+                        console.log('Ошибка при показе рекламы');
                     }
                 })
                 .catch((error) => {
-                    console.log('Ошибка при проверке готовности рекламы:', error);
+                    console.log('Ошибка:', error);
                 });
-        }, 15000); // 15000 мс = 15 секунд
-
+        }, 15000); // 15000 миллисекунд = 15 секунд
     })
     .catch((error) => {
         console.error('Ошибка инициализации VK Bridge:', error);
